d8b395d1b14e36096757d259276d46ee08df8844
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-03-01 01:30:04 +0400

    updated squares to up 64

--- main.js
+++ main.js
@@ -1,39 +1,117 @@
+let divContainer = document.getElementById("container");
+let divCreator = document.createElement("div");
+let container = document.getElementById("container");
+let squares = container.getElementsByClassName("square");
+
+// Create container with div's
 function squireCreator() {
   let i = 0;
   while (i < 256) {
-    const divContainer = document.getElementById("container");
-    const divCreator = document.createElement("div");
+    divContainer = document.getElementById("container");
+    divCreator = document.createElement("div");
     divCreator.setAttribute("class", "square");
+    divCreator.style.setProperty("width", "32px");
+    divCreator.style.setProperty("height", "32px");
     // divCreator.textContent = `${i}`;
     divContainer.appendChild(divCreator);
     i++;
   }
 }
 
+squireCreator();
+
+let isDrawing = false;
+
 function draw(event) {
-  const container = document.getElementById("container");
-  const squares = container.getElementsByClassName("square");
-  for (let square of squares) {
-    if (square === event.target) {
-      square.classList.add("black");
+  if (isDrawing) {
+    container = document.getElementById("container");
+    squares = container.getElementsByClassName("square");
+    for (let square of squares) {
+      if (square === event.target) {
+        square.classList.add("black");
+      }
     }
   }
 }
 
-squireCreator();
+container.addEventListener("mousemove", draw);
+container.addEventListener("mousedown", () => {
+  isDrawing = true;
+});
+container.addEventListener("mouseup", () => {
+  isDrawing = false;
+});
 
-const container = document.getElementById("container");
-container.addEventListener("mouseover", draw);
+let buttonSquarePromt4 = document.getElementById("squarePromt4");
+let buttonSquarePromt8 = document.getElementById("squarePromt8");
+let buttonSquarePromt16 = document.getElementById("squarePromt16");
+let buttonSquarePromt32 = document.getElementById("squarePromt32");
+let buttonSquarePromt64 = document.getElementById("squarePromt64");
 
-const buttonSquarePromt = document.getElementById("squarePromt");
+function squireCreatorButton(row1, row2) {
+  let i = 0;
+  let x = row1 * row2;
+  let y = 512 / row1;
+  while (i < x) {
+    divContainer = document.getElementById("container");
+    divCreator = document.createElement("div");
+    divCreator.setAttribute("class", "square");
+    divCreator.style.setProperty("width", `${y}px`);
+    divCreator.style.setProperty("height", `${y}px`);
 
-function squarePromt() {
-  let numberOfSquares = prompt(
-    "Enter number of squares per side for the new grid"
-  );
-  console.log(numberOfSquares);
+    // divCreator.textContent = `${i}`;
+    divContainer.appendChild(divCreator);
+    i++;
+  }
 }
 
-buttonSquarePromt.addEventListener("click", () => {
-  squarePromt();
+let row1;
+let row2;
+
+let clearButton = document.getElementById("clear");
+
+clearButton.addEventListener("click", () => {
+  removeAllChildNodes(divContainer);
+  squireCreator();
+});
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+buttonSquarePromt4.addEventListener("click", () => {
+  row1 = 4;
+  row2 = 4;
+  removeAllChildNodes(divContainer);
+  squireCreatorButton(row1, row2);
+});
+
+buttonSquarePromt8.addEventListener("click", () => {
+  row1 = 8;
+  row2 = 8;
+  removeAllChildNodes(divContainer);
+  squireCreatorButton(row1, row2);
+});
+
+buttonSquarePromt16.addEventListener("click", () => {
+  row1 = 16;
+  row2 = 16;
+  removeAllChildNodes(divContainer);
+  squireCreatorButton(row1, row2);
+});
+
+buttonSquarePromt32.addEventListener("click", () => {
+  row1 = 32;
+  row2 = 32;
+  removeAllChildNodes(divContainer);
+  squireCreatorButton(row1, row2);
+});
+
+buttonSquarePromt64.addEventListener("click", () => {
+  row1 = 64;
+  row2 = 64;
+  removeAllChildNodes(divContainer);
+  squireCreatorButton(row1, row2);
 });
