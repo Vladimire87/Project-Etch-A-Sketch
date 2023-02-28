bf8dea0bbcd13084256976ebe7296816e0ef96a1
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-03-01 02:34:56 +0400

    footer, code style, readme

--- main.js
+++ main.js
@@ -1,31 +1,26 @@
-let divContainer = document.getElementById("container");
-let divCreator = document.createElement("div");
-let container = document.getElementById("container");
-let squares = container.getElementsByClassName("square");
+const container = document.getElementById("container");
+const squares = container.getElementsByClassName("square");
 
 // Create container with div's
-function squireCreator() {
+function squareCreator() {
   let i = 0;
   while (i < 256) {
-    divContainer = document.getElementById("container");
-    divCreator = document.createElement("div");
-    divCreator.setAttribute("class", "square");
-    divCreator.style.setProperty("width", "32px");
-    divCreator.style.setProperty("height", "32px");
+    const divCreator = document.createElement("div");
+    divCreator.classList.add("square");
+    divCreator.style.width = "32px";
+    divCreator.style.height = "32px";
     // divCreator.textContent = `${i}`;
-    divContainer.appendChild(divCreator);
+    container.appendChild(divCreator);
     i++;
   }
 }
 
-squireCreator();
+squareCreator();
 
 let isDrawing = false;
 
 function draw(event) {
   if (isDrawing) {
-    container = document.getElementById("container");
-    squares = container.getElementsByClassName("square");
     for (let square of squares) {
       if (square === event.target) {
         square.classList.add("black");
@@ -42,38 +37,30 @@ container.addEventListener("mouseup", () => {
   isDrawing = false;
 });
 
-let buttonSquarePromt4 = document.getElementById("squarePromt4");
-let buttonSquarePromt8 = document.getElementById("squarePromt8");
-let buttonSquarePromt16 = document.getElementById("squarePromt16");
-let buttonSquarePromt32 = document.getElementById("squarePromt32");
-let buttonSquarePromt64 = document.getElementById("squarePromt64");
+const buttonSquarePrompts = {
+  4: document.getElementById("squarePromt4"),
+  8: document.getElementById("squarePromt8"),
+  16: document.getElementById("squarePromt16"),
+  32: document.getElementById("squarePromt32"),
+  64: document.getElementById("squarePromt64"),
+};
 
-function squireCreatorButton(row1, row2) {
+function squareCreatorButton(rows, columns) {
+  const totalSquares = rows * columns;
+  const squareSize = 512 / rows;
+
   let i = 0;
-  let x = row1 * row2;
-  let y = 512 / row1;
-  while (i < x) {
-    divContainer = document.getElementById("container");
-    divCreator = document.createElement("div");
-    divCreator.setAttribute("class", "square");
-    divCreator.style.setProperty("width", `${y}px`);
-    divCreator.style.setProperty("height", `${y}px`);
+  while (i < totalSquares) {
+    const divCreator = document.createElement("div");
+    divCreator.classList.add("square");
+    divCreator.style.width = `${squareSize}px`;
+    divCreator.style.height = `${squareSize}px`;
 
     // divCreator.textContent = `${i}`;
-    divContainer.appendChild(divCreator);
+    container.appendChild(divCreator);
     i++;
   }
 }
-
-let row1;
-let row2;
-
-let clearButton = document.getElementById("clear");
-
-clearButton.addEventListener("click", () => {
-  removeAllChildNodes(divContainer);
-  squireCreator();
-});
 
 function removeAllChildNodes(parent) {
   while (parent.firstChild) {
@@ -81,37 +68,17 @@ function removeAllChildNodes(parent) {
   }
 }
 
-buttonSquarePromt4.addEventListener("click", () => {
-  row1 = 4;
-  row2 = 4;
-  removeAllChildNodes(divContainer);
-  squireCreatorButton(row1, row2);
+const clearButton = document.getElementById("clear");
+
+clearButton.addEventListener("click", () => {
+  removeAllChildNodes(container);
+  squareCreator();
 });
 
-buttonSquarePromt8.addEventListener("click", () => {
-  row1 = 8;
-  row2 = 8;
-  removeAllChildNodes(divContainer);
-  squireCreatorButton(row1, row2);
-});
-
-buttonSquarePromt16.addEventListener("click", () => {
-  row1 = 16;
-  row2 = 16;
-  removeAllChildNodes(divContainer);
-  squireCreatorButton(row1, row2);
-});
-
-buttonSquarePromt32.addEventListener("click", () => {
-  row1 = 32;
-  row2 = 32;
-  removeAllChildNodes(divContainer);
-  squireCreatorButton(row1, row2);
-});
-
-buttonSquarePromt64.addEventListener("click", () => {
-  row1 = 64;
-  row2 = 64;
-  removeAllChildNodes(divContainer);
-  squireCreatorButton(row1, row2);
+Object.keys(buttonSquarePrompts).forEach((key) => {
+  const button = buttonSquarePrompts[key];
+  button.addEventListener("click", () => {
+    removeAllChildNodes(container);
+    squareCreatorButton(key, key);
+  });
 });
